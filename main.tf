fe60c5417c454424e61661143eebe42aa512fd1c
Ryano0oceros <getryanfallon@gmail.com>
2021-05-20 15:22:40 -0400

    rogue instance in public subnet

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "rogue_instance" {
   ami                  = data.aws_ami.ubuntu.id
   instance_type        = var.instance_size
-  subnet_id            = module.hashi-demo-vpc-secondary.public_subnets[0]
+  subnet_id            = module.hashi-demo-vpc.public_subnets[0]
   
   tags                 = {"Name" = "rogue_instance"}
 }
